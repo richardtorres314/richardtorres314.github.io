b879dbd47815d0171050904c51e281e9bf11f277
Richard Torres <richardtorres314@gmail.com>
2021-07-15 15:10:55 -0400

    re-exporting

---
+++ _next/static/ZYHKAjW9BhRDcpBXT6VwP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/218-63c57d4f653c1e11b9c8.js","static/chunks/pages/index-900d87ed7e996732217d.js"],"/_error":["static/chunks/pages/_error-94ed2348718d59e1af9b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
