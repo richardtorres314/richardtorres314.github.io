c8084eecee5915e5275867099b577f03ecb1590e
Richard Torres <richardtorres314@gmail.com>
2022-04-13 15:11:28 -0500

    updating experiences

---
+++ docs/_next/static/0DcQ9GLC5JbMRgkgt1Sdt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/aa8918e7c56539a2.css","static/chunks/pages/index-ed232ff3ab28446b.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/scripts/test.js
@@ -0,0 +1,5 @@
+var container = document.getElementsByTagName("body")[0];
+var content = document.createElement("span");
+content.style.color = "red";
+content.innerHTML = "Hello";
+container.appendChild(content);
