1551e045e54d487f84c32e8e8c6c2566e06e589c
Richard Torres <richardtorres314@gmail.com>
2022-07-06 13:20:46 -0400

    reverting site

---
+++ docs/_next/static/YIIp5aC996CJl4I85MCjY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/18eaa89404c27ac6.css","static/chunks/pages/index-5e3467e9e1169d68.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
