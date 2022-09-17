77c2a7ac88cb0fcb5679cd223d9025a93fa05459
Richard Torres <richardtorres314@gmail.com>
2022-09-17 15:54:41 -0400

    updating skills

--- docs/_next/static/GUsB_n4HkBV4TMiRwC6Qc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dd708fb630348325.css","static/chunks/pages/index-a73575a5e8976f0f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
