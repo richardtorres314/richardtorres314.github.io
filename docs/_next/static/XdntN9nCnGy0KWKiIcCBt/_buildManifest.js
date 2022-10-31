9575128332fdcbe73b647796e3f993c02f5b1c3e
Richard Torres <richardtorres314@gmail.com>
2022-10-31 00:57:11 -0400

    updating exported files

--- docs/_next/static/XdntN9nCnGy0KWKiIcCBt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dd708fb630348325.css","static/chunks/pages/index-523301861066dafd.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
