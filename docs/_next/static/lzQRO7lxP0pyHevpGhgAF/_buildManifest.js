4aca8ea5c18c33676bcb92a05347b79fc8a6d2f4
Richard Torres <richardtorres314@gmail.com>
2022-11-09 13:26:20 -0500

    updating styling on header

--- docs/_next/static/lzQRO7lxP0pyHevpGhgAF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b66f76c36dad1484.css","static/chunks/pages/index-788bc23e17d270d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
