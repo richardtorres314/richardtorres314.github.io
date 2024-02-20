363192dda43b2b7f2cf81a742a671b36a10f89c8
Richard Torres <richardtorres314@gmail.com>
2024-02-19 22:20:05 -0500

    fix: adding media query for color scheme

--- public/appearance.js
+++ public/appearance.js
@@ -1,5 +1,12 @@
 (async function appearance() {
   const theme = localStorage.getItem("theme");
+  if (theme === null) {
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (prefersDarkScheme) {
+      localStorage.setItem('theme', 'dark');
+      document.body.classList.add('dark');
+    }
+  }
   if (theme === 'dark') {
     document.body.classList.add('dark');
   }
