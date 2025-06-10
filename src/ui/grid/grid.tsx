9b3a93dfec8fc15098906e5678d34d664a74c866
Richard Torres <richardtorres314@gmail.com>
2025-06-09 22:08:05 -0400

    chore: removing unused component

--- src/ui/grid/grid.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from "@/lib/utils";
-import type { HTMLAttributes } from "react";
-
-export function Grid(props: HTMLAttributes<HTMLDivElement>) {
-  const { className, ...passedProps } = props;
-
-  return <div className={cn("grid gap-2", className)} {...passedProps} />;
-}
